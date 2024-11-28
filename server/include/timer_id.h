560480c982776d6effaa0c5bfdedef815e201357
YuFireWhipser <yu.whisper.personal@gmail.com>
2024-11-28 15:35:51 +0800

    feat: Implement timer_id to create unique identtifier of TimerQueue

---
+++ server/include/timer_id.h
@@ -0,0 +1,18 @@
+#include <cstdint>
+namespace server {
+class Timer;
+
+class TimerId {
+public:
+  TimerId() : timer_(nullptr), sequence_(0) {}
+
+  TimerId(Timer *timer, int64_t seq) : timer_(timer), sequence_(seq) {}
+
+private:
+  Timer *timer_;
+  int64_t sequence_;
+
+  friend class TimerQueue;
+};
+
+} // namespace server
