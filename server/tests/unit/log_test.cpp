d9760ddfa8875a81b04a5bdd1f4780d12841ada9
YuFireWhipser <yu.whisper.personal@gmail.com>
2024-12-03 16:52:48 +0800

    test: refactor Log class tests

--- server/tests/unit/log_test.cpp
+++ server/tests/unit/log_test.cpp
@@ -4,143 +4,149 @@
 #include <fstream>
 #include <gtest/gtest.h>
 #include <sstream>
-#include <string>
+#include <thread>
 
-class LoggerTest : public ::testing::Test {
+namespace server::test {
+
+class LogTestBase : public ::testing::Test {
 protected:
   void SetUp() override {
-
-    if (std::filesystem::exists(test_log_file_)) {
-      std::filesystem::remove(test_log_file_);
-    }
-
-    server::Logger::clearDefaultOutputFile();
+    removeTestFile();
+    Logger::clearDefaultOutputFile();
   }
 
-  void TearDown() override {
+  void TearDown() override { removeTestFile(); }
 
-    if (std::filesystem::exists(test_log_file_)) {
-      std::filesystem::remove(test_log_file_);
+  std::string getTestFileContent() { return readFileContent(testFilePath_); }
+
+  const std::filesystem::path &getTestFilePath() const { return testFilePath_; }
+
+  std::string captureConsoleOutput(const std::function<void()> &logOperation) {
+    testing::internal::CaptureStdout();
+    logOperation();
+    return testing::internal::GetCapturedStdout();
+  }
+
+  std::string captureErrorOutput(const std::function<void()> &logOperation) {
+    testing::internal::CaptureStderr();
+    logOperation();
+    return testing::internal::GetCapturedStderr();
+  }
+
+  void assertContains(const std::string &content, const std::string &expectedText) {
+    EXPECT_TRUE(content.find(expectedText) != std::string::npos)
+        << "Expected to find: " << expectedText << " in: " << content;
+  }
+
+private:
+  const std::filesystem::path testFilePath_{"test.log"};
+
+  void removeTestFile() {
+    if (std::filesystem::exists(testFilePath_)) {
+      std::filesystem::remove(testFilePath_);
     }
   }
 
-  std::string readLogFile(const std::filesystem::path &path) {
+  std::string readFileContent(const std::filesystem::path &path) {
     std::ifstream file(path);
     if (!file.is_open()) {
-      return "";
+      return {};
     }
     std::stringstream buffer;
     buffer << file.rdbuf();
     return buffer.str();
   }
-
-  const std::filesystem::path test_log_file_ = "test.log";
 };
 
-TEST_F(LoggerTest, BasicLogging) {
-  testing::internal::CaptureStdout();
+TEST_F(LogTestBase, ConsoleLogDisplaysMessageAndLevel) {
+  auto output = captureConsoleOutput([] { Logger::log(LogLevel::INFO, "TestMessage"); });
 
-  server::Logger::log(server::LogLevel::INFO, "Test message");
-
-  std::string output = testing::internal::GetCapturedStdout();
-  EXPECT_TRUE(output.find("Test message") != std::string::npos);
-  EXPECT_TRUE(output.find("INFO") != std::string::npos);
+  assertContains(output, "TestMessage");
+  assertContains(output, "INFO");
 }
 
-TEST_F(LoggerTest, LogToFile) {
-  server::Logger::log(server::LogLevel::ERROR, "Error message", test_log_file_);
+TEST_F(LogTestBase, FileLogWritesMessageAndLevel) {
+  Logger::log(LogLevel::ERROR, "ErrorMessage", getTestFilePath());
+  auto fileContent = getTestFileContent();
 
-  std::string log_content = readLogFile(test_log_file_);
-  EXPECT_TRUE(log_content.find("Error message") != std::string::npos);
-  EXPECT_TRUE(log_content.find("ERROR") != std::string::npos);
+  assertContains(fileContent, "ErrorMessage");
+  assertContains(fileContent, "ERROR");
 }
 
-TEST_F(LoggerTest, DefaultOutputFile) {
-  server::Logger::setDefaultOutputFile(test_log_file_);
-  server::Logger::log(server::LogLevel::DEBUG, "Debug message");
+TEST_F(LogTestBase, DefaultFileWritesLogContent) {
+  Logger::setDefaultOutputFile(getTestFilePath());
+  Logger::log(LogLevel::DEBUG, "DebugMessage");
+  auto fileContent = getTestFileContent();
 
-  std::string log_content = readLogFile(test_log_file_);
-  EXPECT_TRUE(log_content.find("Debug message") != std::string::npos);
-  EXPECT_TRUE(log_content.find("DEBUG") != std::string::npos);
+  assertContains(fileContent, "DebugMessage");
+  assertContains(fileContent, "DEBUG");
 }
 
-TEST_F(LoggerTest, ClearDefaultOutputFile) {
-  server::Logger::setDefaultOutputFile(test_log_file_);
-  server::Logger::clearDefaultOutputFile();
-  server::Logger::log(server::LogLevel::INFO, "Test message");
+TEST_F(LogTestBase, ClearedDefaultFileCreatesNoFile) {
+  Logger::setDefaultOutputFile(getTestFilePath());
+  Logger::clearDefaultOutputFile();
+  Logger::log(LogLevel::INFO, "TestMessage");
 
-  EXPECT_FALSE(std::filesystem::exists(test_log_file_));
+  EXPECT_FALSE(std::filesystem::exists(getTestFilePath()));
 }
 
-TEST_F(LoggerTest, MultipleLogLevels) {
-  testing::internal::CaptureStdout();
+TEST_F(LogTestBase, LogFormatContainsRequiredElements) {
+  auto output = captureConsoleOutput([] { Logger::log(LogLevel::INFO, "FormatTest"); });
 
-  server::Logger::log(server::LogLevel::TRACE, "Trace message");
-  server::Logger::log(server::LogLevel::DEBUG, "Debug message");
-  server::Logger::log(server::LogLevel::INFO, "Info message");
-  server::Logger::log(server::LogLevel::WARN, "Warning message");
-  server::Logger::log(server::LogLevel::ERROR, "Error message");
-  server::Logger::log(server::LogLevel::FATAL, "Fatal message");
-
-  std::string output = testing::internal::GetCapturedStdout();
-  EXPECT_TRUE(output.find("TRACE") != std::string::npos);
-  EXPECT_TRUE(output.find("DEBUG") != std::string::npos);
-  EXPECT_TRUE(output.find("INFO") != std::string::npos);
-  EXPECT_TRUE(output.find("WARN") != std::string::npos);
-  EXPECT_TRUE(output.find("ERROR") != std::string::npos);
-  EXPECT_TRUE(output.find("FATAL") != std::string::npos);
+  assertContains(output, "[");
+  assertContains(output, "]");
+  assertContains(output, ".cpp");
+  assertContains(output, ":");
 }
 
-TEST_F(LoggerTest, LogFormatting) {
-  testing::internal::CaptureStdout();
+TEST_F(LogTestBase, SupportsAllLogLevels) {
+  auto output = captureConsoleOutput([] {
+    Logger::log(LogLevel::TRACE, "TraceMessage");
+    Logger::log(LogLevel::DEBUG, "DebugMessage");
+    Logger::log(LogLevel::INFO, "InfoMessage");
+    Logger::log(LogLevel::WARN, "WarnMessage");
+    Logger::log(LogLevel::ERROR, "ErrorMessage");
+    Logger::log(LogLevel::FATAL, "FatalMessage");
+  });
 
-  server::Logger::log(server::LogLevel::INFO, "Test message");
-
-  std::string output = testing::internal::GetCapturedStdout();
-
-  EXPECT_TRUE(output.find("[") != std::string::npos);
-  EXPECT_TRUE(output.find("]") != std::string::npos);
-
-  EXPECT_TRUE(output.find(".cpp") != std::string::npos);
-  EXPECT_TRUE(output.find(":") != std::string::npos);
+  assertContains(output, "TRACE");
+  assertContains(output, "DEBUG");
+  assertContains(output, "INFO");
+  assertContains(output, "WARN");
+  assertContains(output, "ERROR");
+  assertContains(output, "FATAL");
 }
 
-TEST_F(LoggerTest, ConcurrentFileAccess) {
-  const int num_threads = 10;
+TEST_F(LogTestBase, HandlesMultipleThreadsSafely) {
+  const int threadCount = 10;
   std::vector<std::thread> threads;
+  Logger::setDefaultOutputFile(getTestFilePath());
 
-  server::Logger::setDefaultOutputFile(test_log_file_);
-
-  for (int i = 0; i < num_threads; ++i) {
-    threads.emplace_back([i]() {
-      server::Logger::log(server::LogLevel::INFO, "Message from thread " + std::to_string(i));
-    });
+  for (int i = 0; i < threadCount; ++i) {
+    threads.emplace_back([i] { Logger::log(LogLevel::INFO, "Thread" + std::to_string(i)); });
   }
 
   for (auto &thread : threads) {
     thread.join();
   }
 
-  std::string log_content = readLogFile(test_log_file_);
-  for (int i = 0; i < num_threads; ++i) {
-    EXPECT_TRUE(log_content.find("Message from thread " + std::to_string(i)) != std::string::npos);
+  auto fileContent = getTestFileContent();
+  for (int i = 0; i < threadCount; ++i) {
+    assertContains(fileContent, "Thread" + std::to_string(i));
   }
 }
 
-TEST_F(LoggerTest, LogToInvalidFile) {
-  testing::internal::CaptureStderr();
+TEST_F(LogTestBase, HandlesInvalidFilePath) {
+  auto error =
+      captureErrorOutput([] { Logger::log(LogLevel::INFO, "Test", "/invalid/path/test.log"); });
 
-  server::Logger::log(server::LogLevel::INFO, "Test message", "/invalid/path/test.log");
-
-  std::string error_output = testing::internal::GetCapturedStderr();
-  EXPECT_TRUE(error_output.find("Failed to open log file") != std::string::npos);
+  assertContains(error, "Failed to open log file");
 }
 
-TEST_F(LoggerTest, EmptyMessage) {
-  testing::internal::CaptureStdout();
+TEST_F(LogTestBase, HandlesEmptyMessage) {
+  auto output = captureConsoleOutput([] { Logger::log(LogLevel::INFO, ""); });
 
-  server::Logger::log(server::LogLevel::INFO, "");
-
-  std::string output = testing::internal::GetCapturedStdout();
-  EXPECT_TRUE(output.find("INFO") != std::string::npos);
+  assertContains(output, "INFO");
 }
+
+} // namespace server::test
