4ad8a1b5b4f64d9b0fdd21a3cd05dc0c351b20a5
YuFireWhipser <yu.whisper.personal@gmail.com>
2024-12-04 15:06:17 +0800

    fix: change getSockAddr() return to sockaddr(not sockaddr_in)

--- server/include/inter_address.h
+++ server/include/inter_address.h
@@ -14,8 +14,8 @@ public:
   std::string getIpPort() const;
   in_port_t getPort() const;
 
-  const sockaddr_in *getSockAddr() const { return &addr_; }
-  const socklen_t getSockLen() const { return sizeof(addr_); } 
+  const sockaddr *getSockAddr() const { return reinterpret_cast<const sockaddr *>(&addr_); }
+  const socklen_t getSockLen() const { return sizeof(addr_); }
   void setSockAddr(const struct sockaddr_in &addr) { addr_ = addr; }
 
   static bool resolveHostname(const std::string &hostname, InterAddress *result);
