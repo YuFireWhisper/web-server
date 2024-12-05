14fb257d58959149e2362dc846a5c9b5935d6d5a
YuFireWhipser <yu.whisper.personal@gmail.com>
2024-12-05 14:20:18 +0800

    feat: Add Socket class

--- server/include/socket.h
+++ server/include/socket.h
@@ -1,34 +1,72 @@
 #pragma once
 
-#include <include/buffer.h>
-#include <memory>
-
-#include <sys/socket.h>
-#include <sys/types.h>
+#include <cstddef>
+#include <netinet/tcp.h>
+#include <stdexcept>
+#include <string>
 
 namespace server {
+
+class SocketException : public std::runtime_error {
+public:
+  explicit SocketException(const std::string &operation);
+  explicit SocketException(const std::string &operation, int errorCode);
+};
+
+class Buffer;
+class InterAddress;
+
 class Socket {
 public:
-  Socket(int domain = AF_INET);
+  Socket();
+  explicit Socket(int socketFd);
   ~Socket();
 
-  bool bind(const std::string &ip, int port);
-  bool listen(int backlog = SOMAXCONN);
-  std::unique_ptr<Socket> accept();
+  Socket(const Socket &) = delete;
+  Socket &operator=(const Socket &) = delete;
 
-  bool setNonBlocking();
-  bool setReuseAddr();
-  bool setReusePort();
-  bool setKeepAlive();
+  void bindToPort(uint16_t port);
+  void bindToAddress(const InterAddress &address);
+  void startListening(int backlog);
+  Socket acceptNewConnection();
 
-  ssize_t read(Buffer &buffer);
-  ssize_t write(const Buffer &buffer);
+  void enableAddressReuse();
+  void enablePortReuse();
+  void enableKeepAlive();
+  void disableNagle();
+  void enableNonBlocking();
 
-  int getFd() const;
+  void closeWriteEnd();
+
+  int getSocketFd() const { return socketFd_; } 
+
+  struct ConnectionInfo {
+    uint32_t stateCode;
+    uint32_t rtt;
+    uint32_t rttVar;
+    uint32_t congestionWindow;
+    uint32_t retransmits;
+    uint32_t totalRetransmits;
+  };
+
+  ConnectionInfo getConnectionInfo() const;
+  InterAddress getLocalAddress() const;
+  InterAddress getRemoteAddress() const;
+
+  bool hasActiveConnection() const;
+  bool hasError() const;
+
+  size_t readData(Buffer &targetBuffer);
+  size_t writeData(const Buffer &sourceBuffer);
+  size_t writeData(const void *dataPtr, size_t dataLength);
 
 private:
-  int fd_;
-  void close();
+  const int socketFd_;
+
+  static int createTcpSocket();
+  void setSocketFlag(int level, int flag, bool enabled);
+  void configureBlockingMode(bool shouldBlock);
+  int getLastError() const;
 };
 
 } // namespace server
--- server/src/socket.cpp
+++ server/src/socket.cpp
@@ -1,116 +1,256 @@
 #include "include/socket.h"
 
-#include "include/log.h"
+#include "include/buffer.h"
+#include "include/inter_address.h"
 
-#include <arpa/inet.h>
 #include <asm-generic/socket.h>
-#include <bits/types/struct_iovec.h>
+#include <cstring>
 #include <fcntl.h>
-#include <memory>
 #include <netinet/in.h>
-#include <string.h>
+#include <netinet/tcp.h>
+#include <thread>
 #include <unistd.h>
 
-#include <sys/select.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <sys/uio.h>
 
+namespace {
+constexpr int EXTRA_BUFFER_SIZE = 65536;
+constexpr int TEMPORARY_SLEEP_MS = 100;
+} // namespace
+
 namespace server {
-Socket::Socket(int domain) {
-  fd_ = ::socket(domain, SOCK_STREAM, 0);
-  if (fd_ < 0) {
-    Logger::log(LogLevel::ERROR, "Socket creation failed", "socket.log");
+
+SocketException::SocketException(const std::string &operation)
+    : std::runtime_error(operation + " failed") {}
+
+SocketException::SocketException(const std::string &operation, int errorCode)
+    : std::runtime_error(operation + " failed: " + std::strerror(errorCode)) {}
+
+Socket::Socket()
+    : socketFd_(createTcpSocket()) {}
+
+Socket::Socket(int socketFd)
+    : socketFd_(socketFd) {
+  if (socketFd_ < 0) {
+    throw SocketException("Socket creation");
   }
 }
 
 Socket::~Socket() {
-  close();
+  ::close(socketFd_);
 }
 
-void Socket::close() {
-  if (fd_ >= 0) {
-    ::close(fd_);
-    fd_ = -1;
+int Socket::createTcpSocket() {
+  int fd = ::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, IPPROTO_TCP);
+  if (fd < 0) {
+    throw SocketException("Socket creation", errno);
+  }
+  return fd;
+}
+
+void Socket::bindToPort(uint16_t port) {
+  InterAddress address(port);
+  bindToAddress(address);
+}
+
+void Socket::bindToAddress(const InterAddress &address) {
+  int result = ::bind(socketFd_, address.getSockAddr(), address.getSockLen());
+  if (result < 0) {
+    throw SocketException("Socket bind", errno);
   }
 }
 
-bool Socket::bind(const std::string &ip, int port) {
-  struct sockaddr_in addr;
-  memset(&addr, 0, sizeof(addr));
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(port);
-  addr.sin_addr.s_addr = inet_addr(ip.c_str());
-
-  return ::bind(fd_, (struct sockaddr *)&addr, sizeof(addr)) == 0;
+void Socket::startListening(int backlog) {
+  int result = ::listen(socketFd_, backlog);
+  if (result < 0) {
+    throw SocketException("Socket listen", errno);
+  }
 }
 
-bool Socket::listen(int backlog) {
-  return ::listen(fd_, backlog) == 0;
+Socket Socket::acceptNewConnection() {
+  while (true) {
+    int newFd = ::accept4(socketFd_, nullptr, nullptr, SOCK_NONBLOCK | SOCK_CLOEXEC);
+
+    if (newFd >= 0) {
+      return Socket(newFd);
+    }
+
+    if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(TEMPORARY_SLEEP_MS));
+      continue;
+    }
+
+    throw SocketException("Socket accept", errno);
+  }
 }
 
-std::unique_ptr<Socket> Socket::accept() {
-  struct sockaddr_in addr;
+void Socket::setSocketFlag(int level, int flag, bool enabled) {
+    int value = enabled ? 1 : 0;
+    int result = ::setsockopt(socketFd_, level, flag, &value, sizeof(value));
+    if (result < 0) {
+        throw SocketException("Socket flag configuration", errno);
+    }
+}
+
+void Socket::enableAddressReuse() {
+  setSocketFlag(SOL_SOCKET, SO_REUSEADDR, true);
+}
+
+void Socket::enablePortReuse() {
+  setSocketFlag(SOL_SOCKET, SO_REUSEPORT, true);
+}
+
+void Socket::enableKeepAlive() {
+  setSocketFlag(SOL_SOCKET, SO_KEEPALIVE, true);
+}
+
+void Socket::disableNagle() {
+  setSocketFlag(IPPROTO_TCP, TCP_NODELAY, true);
+}
+
+void Socket::configureBlockingMode(bool shouldBlock) {
+  int flags = fcntl(socketFd_, F_GETFL, 0);
+  if (flags < 0) {
+    throw SocketException("Get socket flags", errno);
+  }
+
+  flags = shouldBlock ? (flags & ~O_NONBLOCK) : (flags | O_NONBLOCK);
+
+  int result = fcntl(socketFd_, F_SETFL, flags);
+  if (result < 0) {
+    throw SocketException("Set socket flags", errno);
+  }
+}
+
+void Socket::enableNonBlocking() {
+  configureBlockingMode(false);
+}
+
+void Socket::closeWriteEnd() {
+  int result = ::shutdown(socketFd_, SHUT_WR);
+  if (result < 0) {
+    throw SocketException("Socket shutdown", errno);
+  }
+}
+
+Socket::ConnectionInfo Socket::getConnectionInfo() const {
+  struct tcp_info rawInfo;
+  socklen_t len = sizeof(rawInfo);
+
+  int result = ::getsockopt(socketFd_, SOL_TCP, TCP_INFO, &rawInfo, &len);
+  if (result < 0) {
+    throw SocketException("Get connection info", errno);
+  }
+
+  return ConnectionInfo{
+      rawInfo.tcpi_state,
+      rawInfo.tcpi_rtt,
+      rawInfo.tcpi_rttvar,
+      rawInfo.tcpi_snd_cwnd,
+      rawInfo.tcpi_retransmits,
+      rawInfo.tcpi_total_retrans
+  };
+}
+
+InterAddress Socket::getLocalAddress() const {
+  sockaddr_in addr{};
   socklen_t len = sizeof(addr);
-  int connfd = ::accept(fd_, (struct sockaddr *)&addr, &len);
 
-  if (connfd >= 0) {
-    return std::make_unique<Socket>(connfd);
+  int result = ::getsockname(socketFd_, reinterpret_cast<sockaddr *>(&addr), &len);
+  if (result < 0) {
+    throw SocketException("Get local address", errno);
   }
 
-  return nullptr;
+  return InterAddress(addr);
 }
 
-bool Socket::setNonBlocking() {
-  int flags = fcntl(fd_, F_GETFL, 0);
-  if (flags < 0)
+InterAddress Socket::getRemoteAddress() const {
+  sockaddr_in addr{};
+  socklen_t len = sizeof(addr);
+
+  int result = ::getpeername(socketFd_, reinterpret_cast<sockaddr *>(&addr), &len);
+  if (result < 0) {
+    throw SocketException("Get remote address", errno);
+  }
+
+  return InterAddress(addr);
+}
+
+bool Socket::hasActiveConnection() const {
+  try {
+    return getConnectionInfo().stateCode == TCP_ESTABLISHED;
+  } catch (const SocketException &) {
     return false;
-  flags |= O_NONBLOCK;
-  return fcntl(fd_, F_SETFL, flags) == 0;
+  }
 }
 
-bool Socket::setReuseAddr() {
-  int optval = 1;
-  return setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == 0;
+int Socket::getLastError() const {
+  int error = 0;
+  socklen_t len = sizeof(error);
+  ::getsockopt(socketFd_, SOL_SOCKET, SO_ERROR, &error, &len);
+  return error;
 }
 
-bool Socket::setReusePort() {
-  int optval = 1;
-  return setsockopt(fd_, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval)) == 0;
+bool Socket::hasError() const {
+  return getLastError() != 0;
 }
 
-bool Socket::setKeepAlive() {
-  int optval = 1;
-  return setsockopt(fd_, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof(optval)) == 0;
-}
-
-ssize_t Socket::read(Buffer &buffer) {
-  char extrabuf[65536];
+size_t Socket::readData(Buffer &targetBuffer) {
+  char extraBuffer[EXTRA_BUFFER_SIZE];
   struct iovec vec[2];
-  const size_t writable = buffer.writableBytes();
-  vec[0].iov_base = buffer.beginWrite();
-  vec[0].iov_len = writable;
-  vec[1].iov_base = extrabuf;
-  vec[1].iov_len = sizeof(extrabuf);
 
-  const ssize_t n = readv(fd_, vec, 2);
-  if (n < 0) {
-    return n;
-  } else if (static_cast<size_t>(n) <= writable) {
-    buffer.hasWritten(n);
-  } else {
-    buffer.hasWritten(writable);
-    buffer.append(extrabuf, n - writable);
+  const size_t mainSpace = targetBuffer.writableBytes();
+  vec[0].iov_base = targetBuffer.beginWrite();
+  vec[0].iov_len = mainSpace;
+  vec[1].iov_base = extraBuffer;
+  vec[1].iov_len = sizeof(extraBuffer);
+
+  const int vectorCount = (mainSpace < sizeof(extraBuffer)) ? 2 : 1;
+  const ssize_t bytesRead = ::readv(socketFd_, vec, vectorCount);
+
+  if (bytesRead < 0) {
+    if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR) {
+      return 0;
+    }
+    throw SocketException("Socket read", errno);
   }
 
-  return n;
+  if (static_cast<size_t>(bytesRead) <= mainSpace) {
+    targetBuffer.hasWritten(bytesRead);
+  } else {
+    targetBuffer.hasWritten(mainSpace);
+    targetBuffer.append(extraBuffer, bytesRead - mainSpace);
+  }
+
+  return bytesRead;
 }
 
-ssize_t Socket::write(const Buffer &buffer) {
-  return ::write(fd_, buffer.peek(), buffer.writableBytes());
+size_t Socket::writeData(const Buffer &sourceBuffer) {
+  return writeData(sourceBuffer.peek(), sourceBuffer.readableBytes());
 }
 
-int Socket::getFd() const {
-  return fd_;
-};
+size_t Socket::writeData(const void *dataPtr, size_t dataLength) {
+  size_t bytesSent = 0;
+  const char *currentPtr = static_cast<const char *>(dataPtr);
+
+  while (bytesSent < dataLength) {
+    ssize_t result = ::write(socketFd_, currentPtr + bytesSent, dataLength - bytesSent);
+
+    if (result < 0) {
+      if (errno == EINTR) {
+        continue;
+      }
+      if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        break;
+      }
+      throw SocketException("Socket write", errno);
+    }
+
+    bytesSent += result;
+  }
+
+  return bytesSent;
+}
+
 } // namespace server
