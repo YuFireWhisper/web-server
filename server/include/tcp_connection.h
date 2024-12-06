aa01efa175b90d54298fc4a22005b0e7aff39717
YuFireWhipser <yu.whisper.personal@gmail.com>
2024-12-06 18:36:19 +0800

    feat: Implement TcpConnection class

---
+++ server/include/tcp_connection.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "buffer.h"
+#include "channel.h"
+#include "event_loop.h"
+#include "inet_address.h"
+#include "socket.h"
+#include "types.h"
+
+#include <functional>
+#include <memory>
+namespace server {
+class TcpConnection;
+
+using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+
+class TcpConnection : public std::enable_shared_from_this<TcpConnection> {
+public:
+  using ConnectionCallback = std::function<void(const TcpConnectionPtr &)>;
+  using MessageCallback = std::function<void(const TcpConnectionPtr &, Buffer *, TimeStamp)>;
+  using WriteCompleteCallback = std::function<void(const TcpConnectionPtr &)>;
+  using CloseCallback = std::function<void(const TcpConnectionPtr &)>;
+  using HighWaterMarkCallback = std::function<void(const TcpConnectionPtr &, size_t)>;
+  using ErrorCallback = std::function<void(const TcpConnectionPtr &)>;
+
+  enum class State { kDisconnected, kConnecting, kConnected, kDisconnecting };
+
+  TcpConnection(
+      EventLoop *loop,
+      std::string name,
+      std::unique_ptr<Socket> socket,
+      const InetAddress &localAddr,
+      const InetAddress &peerAddr
+  );
+
+  ~TcpConnection();
+
+  TcpConnection(const TcpConnection &) = delete;
+  TcpConnection &operator=(const TcpConnection &) = delete;
+
+  EventLoop *getLoop() const { return loop_; }
+  const std::string &name() const { return name_; }
+  const InetAddress &localAddress() const { return localAddr_; }
+  const InetAddress &peerAddress() const { return peerAddr_; }
+  bool connected() const { return state_ == State::kConnected; }
+
+  void send(const std::string &message);
+  void send(std::string_view message);
+  void send(Buffer *buffer);
+
+  void shutdown();
+  void forceClose();
+
+  void setConnectionCallback(const ConnectionCallback &cb) { connectionCallback_ = cb; }
+  void setMessageCallback(const MessageCallback &cb) { messageCallback_ = cb; }
+  void setWriteCompleteCallback(const WriteCompleteCallback &cb) { writeCompleteCallback_ = cb; }
+  void setCloseCallback(const CloseCallback &cb) { closeCallback_ = cb; }
+  void setErrorCallback(const ErrorCallback &cb) { errorCallback_ = cb; }
+  void setHighWaterMarkCallback(const HighWaterMarkCallback &cb, size_t highWaterMark) {
+    highWaterMarkCallback_ = cb;
+    highWaterMark_ = highWaterMark;
+  }
+
+  void connectEstablished();
+  void connectDestroyed();
+
+private:
+  void handleRead(TimeStamp receiveTime);
+  void handleWrite();
+  void handleClose();
+  void handleError();
+
+  void sendInLoop(const void *message, size_t len);
+  void shutdownInLoop();
+  void forceCloseInLoop();
+
+  void setState(State s) { state_ = s; }
+
+  EventLoop *loop_;
+  const std::string name_;
+  State state_;
+
+  std::unique_ptr<Socket> socket_;
+  std::unique_ptr<Channel> channel_;
+  const InetAddress localAddr_;
+  const InetAddress peerAddr_;
+
+  Buffer inputBuffer_;
+  Buffer outputBuffer_;
+
+  ConnectionCallback connectionCallback_ = [](const TcpConnectionPtr &) {};
+  MessageCallback messageCallback_ = [](const TcpConnectionPtr &, Buffer *, TimeStamp) {};
+  WriteCompleteCallback writeCompleteCallback_ = [](const TcpConnectionPtr &) {};
+  CloseCallback closeCallback_ = [](const TcpConnectionPtr &) {};
+  HighWaterMarkCallback highWaterMarkCallback_ = [](const TcpConnectionPtr &, size_t) {};
+  ErrorCallback errorCallback_ = [](const TcpConnectionPtr &) {};
+  size_t highWaterMark_ = 64 * 1024 * 1024;
+};
+
+} // namespace server
---
+++ server/src/tcp_connection.cpp
@@ -0,0 +1,318 @@
+#include "include/tcp_connection.h"
+
+#include "include/event_loop.h"
+#include "include/log.h"
+#include "include/poller.h"
+
+#include <cassert>
+#include <cerrno>
+#include <cstddef>
+#include <cstring>
+#include <memory>
+#include <string_view>
+#include <unistd.h>
+
+#include <sys/types.h>
+namespace server {
+
+TcpConnection::TcpConnection(
+    EventLoop *loop,
+    std::string name,
+    std::unique_ptr<Socket> socket,
+    const InetAddress &localAddr,
+    const InetAddress &peerAddr
+)
+    : loop_(loop)
+    , name_(std::move(name))
+    , state_(State::kConnecting)
+    , socket_(std::move(socket))
+    , localAddr_(localAddr)
+    , peerAddr_(peerAddr)
+    , channel_(std::make_unique<Channel>(loop, socket_->getSocketFd())) {
+  socket_->enableNonBlocking();
+  socket_->enableKeepAlive();
+
+  channel_->setReadCallback(std::bind(&TcpConnection::handleRead, this, std::placeholders::_1));
+  channel_->setWriteCallback(std::bind(&TcpConnection::handleWrite, this));
+  channel_->setCloseCallback(std::bind(&TcpConnection::handleClose, this));
+  channel_->setErrorCallback(std::bind(&TcpConnection::handleError, this));
+
+  Logger::log(
+      LogLevel::INFO,
+      "TcpConnection::ctor[" + name_ + "] at fd=" + std::to_string(socket_->getSocketFd())
+  );
+}
+
+TcpConnection::~TcpConnection() {
+  channel_->disableAll();
+  channel_->remove();
+
+  if (socket_->hasActiveConnection()) {
+    Logger::log(LogLevel::WARN, "Connect did not close properly!");
+  }
+}
+
+void TcpConnection::send(const std::string &message) {
+  if (loop_->isInLoopThread()) {
+    sendInLoop(message.data(), message.size());
+  } else {
+    loop_->runInLoop([this, message]() { sendInLoop(message.data(), message.size()); });
+  }
+}
+
+void TcpConnection::send(std::string_view message) {
+  sendInLoop(message.data(), message.size());
+}
+
+void TcpConnection::send(Buffer *buffer) {
+  if (!buffer) {
+    Logger::log(LogLevel::ERROR, "Attempting to send null buffer");
+    return;
+  }
+
+  if (loop_->isInLoopThread()) {
+    sendInLoop(buffer->peek(), buffer->readableBytes());
+    buffer->retrieveAll();
+  } else {
+    std::string message(buffer->peek(), buffer->readableBytes());
+    loop_->runInLoop([this, message]() { sendInLoop(message.data(), message.size()); });
+    buffer->retrieveAll();
+  }
+}
+
+void TcpConnection::sendInLoop(const void *data, size_t len) {
+  loop_->assertInLoopThread();
+
+  Logger::log(
+      LogLevel::INFO,
+      name_ + ": sendInLoop attempting to write " + std::to_string(len) + " bytes"
+  );
+
+  if (len >= highWaterMark_ && highWaterMarkCallback_) {
+    Logger::log(LogLevel::INFO, name_ + ": High water mark reached before write");
+    loop_->queueInLoop(std::bind(highWaterMarkCallback_, shared_from_this(), len));
+  }
+
+  ssize_t nwrote = 0;
+  size_t remaining = len;
+  bool faultError = false;
+
+  if (!channel_->isWriting() && outputBuffer_.readableBytes() == 0) {
+    nwrote = ::write(channel_->fd(), data, len);
+    Logger::log(LogLevel::INFO, name_ + ": Write result: " + std::to_string(nwrote) + " bytes");
+
+    if (nwrote >= 0) {
+      remaining = len - nwrote;
+      Logger::log(LogLevel::INFO, name_ + ": Remaining bytes: " + std::to_string(remaining));
+
+      if (remaining == 0 && writeCompleteCallback_) {
+        loop_->queueInLoop(std::bind(writeCompleteCallback_, shared_from_this()));
+      }
+    } else {
+      if (errno != EWOULDBLOCK) {
+        Logger::log(LogLevel::ERROR, name_ + ": Write error: " + std::string(strerror(errno)));
+        if (errno == EPIPE || errno == ECONNRESET) {
+          faultError = true;
+        }
+      }
+    }
+  }
+
+  if (!faultError && remaining > 0) {
+    size_t oldLen = outputBuffer_.readableBytes();
+    outputBuffer_.append(static_cast<const char *>(data) + nwrote, remaining);
+
+    if (!channel_->isWriting()) {
+      channel_->enableWriting();
+    }
+  }
+}
+
+void TcpConnection::shutdown() {
+  if (state_ == State::kConnected) {
+    setState(State::kDisconnecting);
+    loop_->runInLoop(std::bind(&TcpConnection::shutdownInLoop, shared_from_this()));
+  }
+}
+
+void TcpConnection::shutdownInLoop() {
+  loop_->assertInLoopThread();
+
+  if (!channel_->isWriting()) {
+    socket_->closeWriteEnd();
+
+    channel_->disableAll();
+
+    setState(State::kDisconnected);
+
+    if (closeCallback_) {
+      Logger::log(LogLevel::INFO, name_ + ": Triggering close callback after shutdown");
+      closeCallback_(shared_from_this());
+    }
+
+    channel_->remove();
+  }
+}
+
+void TcpConnection::forceClose() {
+  if (loop_->isInLoopThread()) {
+    forceCloseInLoop();
+  } else {
+    loop_->runInLoop(std::bind(&TcpConnection::forceCloseInLoop, shared_from_this()));
+  }
+}
+
+void TcpConnection::forceCloseInLoop() {
+  loop_->assertInLoopThread();
+
+  if (state_ == State::kConnected || state_ == State::kDisconnecting) {
+    setState(State::kDisconnected);
+
+    channel_->disableAll();
+
+    if (connectionCallback_) {
+      connectionCallback_(shared_from_this());
+    }
+
+    socket_->closeWriteEnd();
+
+    if (closeCallback_) {
+      closeCallback_(shared_from_this());
+    }
+    channel_->remove();
+  }
+}
+
+void TcpConnection::handleWrite() {
+  loop_->assertInLoopThread();
+
+  if (channel_->isWriting()) {
+    ssize_t n = ::write(channel_->fd(), outputBuffer_.peek(), outputBuffer_.readableBytes());
+
+    if (n > 0) {
+      outputBuffer_.retrieve(n);
+      if (outputBuffer_.readableBytes() == 0) {
+        channel_->disableWriting();
+
+        if (writeCompleteCallback_) {
+          loop_->queueInLoop(std::bind(writeCompleteCallback_, shared_from_this()));
+        }
+
+        if (state_ == State::kDisconnecting) {
+          shutdownInLoop();
+        }
+      }
+    }
+  }
+}
+
+void TcpConnection::handleClose() {
+  loop_->assertInLoopThread();
+
+  if (state_ != State::kDisconnected) {
+    setState(State::kDisconnected);
+
+    channel_->disableAll();
+
+    if (connectionCallback_) {
+      connectionCallback_(shared_from_this());
+    }
+
+    if (channel_) {
+      channel_->remove();
+    }
+
+    if (closeCallback_) {
+      closeCallback_(shared_from_this());
+    }
+  }
+}
+
+void TcpConnection::handleError() {
+  int err = 0;
+  socklen_t errlen = sizeof(err);
+
+  if (::getsockopt(channel_->fd(), SOL_SOCKET, SO_ERROR, &err, &errlen) == 0) {
+    std::string errMsg = strerror(err);
+
+    switch (err) {
+      case ECONNRESET:
+        Logger::log(LogLevel::ERROR, name_ + ": Conection reset by peer: " + errMsg);
+        handleClose();
+        break;
+
+      case ETIMEDOUT:
+        Logger::log(LogLevel::ERROR, name_ + ": Connection timed out: " + errMsg);
+        handleClose();
+        break;
+
+      case EPIPE:
+        Logger::log(LogLevel::ERROR, name_ + ": Broken pipe: " + errMsg);
+        handleClose();
+        break;
+
+      default:
+        Logger::log(
+            LogLevel::ERROR,
+            name_ + ": Socket error: " + errMsg + " (error code: " + std::to_string(err) + ")"
+        );
+
+        if (!socket_->hasActiveConnection()) {
+          handleClose();
+        }
+        break;
+    }
+
+    if (errorCallback_) {
+      errorCallback_(shared_from_this());
+    }
+  } else {
+    Logger::log(LogLevel::ERROR, name_ + ": Failed to get socket error");
+  }
+}
+
+void TcpConnection::handleRead(TimeStamp receiveTime) {
+  int savedErrno = 0;
+  ssize_t n = inputBuffer_.readData(socket_->getSocketFd(), &savedErrno);
+
+  if (n > 0) {
+    messageCallback_(shared_from_this(), &inputBuffer_, receiveTime);
+  } else if (n == 0) {
+    handleClose();
+  } else {
+    errno = savedErrno;
+    Logger::log(LogLevel::ERROR, "Read Error" + std::string(strerror(errno)));
+    handleError();
+  }
+}
+
+void TcpConnection::connectEstablished() {
+  loop_->assertInLoopThread();
+
+  assert(state_ == State::kConnecting);
+  setState(State::kConnected);
+
+  channel_->enableReading();
+
+  channel_->setReadCallback(std::bind(&TcpConnection::handleRead, this, std::placeholders::_1));
+  channel_->setWriteCallback(std::bind(&TcpConnection::handleWrite, this));
+  channel_->setCloseCallback(std::bind(&TcpConnection::handleClose, this));
+  channel_->setErrorCallback(std::bind(&TcpConnection::handleError, this));
+
+  if (connectionCallback_) {
+    connectionCallback_(shared_from_this());
+  }
+}
+
+void TcpConnection::connectDestroyed() {
+  loop_->assertInLoopThread();
+
+  if (state_ == State::kConnected) {
+    setState(State::kDisconnected);
+    channel_->disableAll();
+    connectionCallback_(shared_from_this());
+    channel_->remove();
+  }
+}
+
+} // namespace server
