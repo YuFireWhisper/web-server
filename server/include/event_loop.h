c5cbb624fef8f2b2641b1290555cb13cc1059679
YuFireWhipser <yu.whisper.personal@gmail.com>
2024-12-24 20:46:46 +0800

    refactor: Refactor EventLoop

--- server/include/event_loop.h
+++ server/include/event_loop.h
@@ -1,11 +1,10 @@
 #pragma once
 
+#include "include/config_defaults.h"
 #include "include/time_stamp.h"
 #include "include/types.h"
 
 #include <atomic>
-#include <memory>
-#include <mutex>
 #include <pthread.h>
 #include <vector>
 
@@ -19,6 +18,9 @@ public:
   EventLoop();
   ~EventLoop();
 
+  EventLoop(const EventLoop &)            = delete;
+  EventLoop &operator=(const EventLoop &) = delete;
+
   void loop();
   void quit();
 
@@ -28,42 +30,37 @@ public:
   void updateChannel(Channel *channel);
   void removeChannel(Channel *channel);
 
-  [[nodiscard]] bool isInLoopThread() const {
-    return pthread_equal(threadId_, pthread_self()) != 0;
-  }
+  [[nodiscard]] bool isInLoopThread() const { return threadId_ == pthread_self(); }
   [[nodiscard]] bool isWakeupFd(int fd) const { return fd == wakeupFd_; }
   [[nodiscard]] int getWakeupFd() const { return wakeupFd_; }
+  [[nodiscard]] Poller *getPoller() const { return poller_; }
 
   void handleWakeup();
   void assertInLoopThread() const;
 
-  [[nodiscard]] Poller *getPoller() const { return poller_.get(); }
-
 private:
   void doPendingFunctors();
-  static int createEventfd();
   void writeToWakeupFd() const;
   void readFromWakeupFd() const;
+  static int createEventfd();
 
-  std::atomic<bool> looping_{false};
-  std::atomic<bool> quit_{false};
-  std::atomic<bool> eventHandling_{false};
-  std::atomic<bool> callingPendingFunctors_{false};
+  std::atomic<bool> looping_{ false };
+  std::atomic<bool> quit_{ false };
+  std::atomic<bool> eventHandling_{ false };
+  std::atomic<bool> callingPendingFunctors_{ false };
 
   const pthread_t threadId_;
-  std::unique_ptr<Poller> poller_;
+  Poller *poller_;
+  const int wakeupFd_;
+  Channel *wakeupChannel_;
 
-  std::atomic<int> wakeupFd_;
-  std::unique_ptr<Channel> wakeupChannel_;
-
-  std::mutex mutex_;
+  mutable pthread_mutex_t mutex_;
   std::vector<Functor> pendingFunctors_;
   ChannelList activeChannels_;
-
   TimeStamp pollReturnTime_;
 
-  static constexpr int kPollTimeMs = 1000;
-  static constexpr uint64_t kWakeupNumber = 1;
+  const GlobalConfig &config_;
+  static constexpr uint64_t WAKE_UP_EVENT = 1;
 };
 
 } // namespace server
--- server/src/event_loop.cpp
+++ server/src/event_loop.cpp
@@ -1,20 +1,38 @@
 #include "include/event_loop.h"
 
 #include "include/channel.h"
+#include "include/config_manager.h"
 #include "include/epoll_poller.h"
+#include "include/log.h"
 
+#include <cstring>
 #include <stdexcept>
+#include <string>
 #include <unistd.h>
 
 #include <sys/eventfd.h>
 
 namespace server {
 
+namespace {
+thread_local EventLoop *loopInThisThread = nullptr;
+}
+
 EventLoop::EventLoop()
     : threadId_(pthread_self())
     , poller_(new EPollPoller(this))
     , wakeupFd_(createEventfd())
-    , wakeupChannel_(new Channel(this, wakeupFd_)) {
+    , wakeupChannel_(new Channel(this, wakeupFd_))
+    , config_(*ConfigManager::getInstance().getCurrentContext().globalContext->conf) {
+
+  if (loopInThisThread != nullptr) {
+    throw std::runtime_error("Another EventLoop exists in this thread");
+  }
+  loopInThisThread = this;
+
+  pthread_mutex_init(&mutex_, nullptr);
+
+  // Initialize wake up channel
   wakeupChannel_->setReadCallback([this](const TimeStamp &) { readFromWakeupFd(); });
   wakeupChannel_->enableReading();
 }
@@ -23,19 +41,28 @@ EventLoop::~EventLoop() {
   wakeupChannel_->disableAll();
   wakeupChannel_->remove();
   ::close(wakeupFd_);
+
+  delete wakeupChannel_;
+  delete poller_;
+
+  pthread_mutex_destroy(&mutex_);
+  loopInThisThread = nullptr;
 }
 
 void EventLoop::loop() {
+  assertInLoopThread();
+
   looping_ = true;
   quit_    = false;
 
   while (!quit_) {
     activeChannels_.clear();
-    pollReturnTime_ = poller_->poll(kPollTimeMs, &activeChannels_);
+    pollReturnTime_ = poller_->poll(config_.pollTimeoutMs, &activeChannels_);
 
-    eventHandling_ = true;
-    for (Channel *channel : activeChannels_) {
-      channel->handleEvent(pollReturnTime_);
+    eventHandling_           = true;
+    const size_t numChannels = activeChannels_.size();
+    for (size_t i = 0; i < numChannels; ++i) {
+      activeChannels_[i]->handleEvent(pollReturnTime_);
     }
     eventHandling_ = false;
 
@@ -61,10 +88,9 @@ void EventLoop::runInLoop(Functor cb) {
 }
 
 void EventLoop::queueInLoop(Functor cb) {
-  {
-    std::lock_guard<std::mutex> lock(mutex_);
-    pendingFunctors_.push_back(std::move(cb));
-  }
+  pthread_mutex_lock(&mutex_);
+  pendingFunctors_.emplace_back(std::move(cb));
+  pthread_mutex_unlock(&mutex_);
 
   if (!isInLoopThread() || callingPendingFunctors_) {
     writeToWakeupFd();
@@ -72,10 +98,12 @@ void EventLoop::queueInLoop(Functor cb) {
 }
 
 void EventLoop::updateChannel(Channel *channel) {
+  assertInLoopThread();
   poller_->updateChannel(channel);
 }
 
 void EventLoop::removeChannel(Channel *channel) {
+  assertInLoopThread();
   poller_->removeChannel(channel);
 }
 
@@ -83,37 +111,37 @@ void EventLoop::doPendingFunctors() {
   std::vector<Functor> functors;
   callingPendingFunctors_ = true;
 
-  {
-    std::lock_guard<std::mutex> lock(mutex_);
-    functors.swap(pendingFunctors_);
-  }
+  pthread_mutex_lock(&mutex_);
+  functors.swap(pendingFunctors_);
+  pthread_mutex_unlock(&mutex_);
 
-  for (const auto &functor : functors) {
-    functor();
+  const size_t numFunctors = functors.size();
+  for (size_t i = 0; i < numFunctors; ++i) {
+    functors[i]();
   }
-
   callingPendingFunctors_ = false;
 }
 
 int EventLoop::createEventfd() {
-  int evtfd = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+  const int evtfd = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
   if (evtfd < 0) {
-    throw std::runtime_error("Failed to create event fd");
+    throw std::runtime_error("Failed to create event fd: " + std::string(strerror(errno)));
   }
   return evtfd;
 }
 
 void EventLoop::writeToWakeupFd() const {
-  uint64_t one = kWakeupNumber;
+  const uint64_t one = WAKE_UP_EVENT;
   if (::write(wakeupFd_, &one, sizeof one) != sizeof one) {
-    throw std::runtime_error("Failed to write to wakeup fd");
+    LOG_ERROR("Failed to write to wakeup fd");
   }
 }
 
 void EventLoop::readFromWakeupFd() const {
   uint64_t one = 0;
   if (::read(wakeupFd_, &one, sizeof one) != sizeof one) {
-    throw std::runtime_error("Failed to read from wakeup fd");
+    // Only log error, don't throw in wake up path
+    LOG_ERROR("Failed to read from wakeup fd");
   }
 }
 
