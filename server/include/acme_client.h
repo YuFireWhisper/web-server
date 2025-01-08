85ab5fc58d0218fbc56861a696e8db2bda429aa3
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-01-08 10:01:50 +0800

    refactor: Remove ACMEClient deprecated member

--- server/include/acme_client.h
+++ server/include/acme_client.h
@@ -39,8 +39,6 @@ public:
   int createCertificate();
   int validateChallenge(const std::string &type);
 
-  [[deprecated]] void registerAccount();
-  [[deprecated]] void requestNewOrder();
   bool requestChallengeCompletion(const std::string &type);
   void requestFinalization();
 
@@ -53,24 +51,9 @@ public:
       std::string *headerData = nullptr
   );
 
-  [[deprecated]] static std::string sendRequest_(
-      std::string_view url,
-      std::string_view data                   = "",
-      const std::vector<std::string> &headers = {}
-  );
-  [[deprecated]] static void sendRequest(
-      std::string_view url,
-      std::string_view data,
-      std::string *response,
-      std::string *headerData,
-      const std::vector<std::string> &headers
-  );
-  [[deprecated]] static std::string
-  sendHeadRequest(std::string_view url, const std::vector<std::string> &headers = {});
   static std::string sendHeadRequest(const std::string &url);
 
   static AcmeUrls getUrls(const std::string &apiUrl);
-  [[deprecated]] static inline std::unordered_map<std::string, AcmeUrls> urlCache_;
 
 private:
   std::string getAccountUrl();
@@ -82,23 +65,10 @@ private:
   void handleOrderPending(const std::string &authzUrl, const std::string &type);
   void handleOrderReady(const std::string &finalizeUrl);
 
-  [[deprecated]] [[nodiscard]] nlohmann::json
-  createAccountRequestHeader(const nlohmann::json &jwk, const std::string &nonce) const;
   [[nodiscard]] nlohmann::json createAccountRequestPayload() const;
-  [[deprecated]] [[nodiscard]] std::string sendAccountRequest(const std::string &jwtToken) const;
-  [[deprecated]] void storeAccountUrl(const std::string &url) const;
-  [[deprecated]] [[nodiscard]] std::tuple<std::string, std::string, std::string>
-  sendOrderRequest(const std::string &jwtToken) const;
   nlohmann::json createHeaderWithJwk(const std::string &url);
   nlohmann::json createHeaderWithKid(const std::string &url);
-  [[deprecated]] static nlohmann::json createHeaderWithKid_(
-      int32_t algNid,
-      const std::string &kid,
-      const std::string &nonce,
-      const std::string &url
-  );
   [[nodiscard]] static nlohmann::json createRequestBody(const std::string &jwtToken);
-  [[deprecated]] static std::string extractLocationHeader(const std::string &response);
   static std::string extractHeaderValue(const std::string &response, const std::string &headerName);
 
   void getChallengeAndDisplay(const std::string &authorizationUrl);
@@ -109,12 +79,9 @@ private:
       const std::string &jwtToken,
       const std::string &finalizeUrl
   );
-  [[deprecated]] [[nodiscard]] nlohmann::json
-  createOrderRequestHeader(const std::string &accountUrl, const std::string &nonce) const;
   [[nodiscard]] nlohmann::json createOrderRequestPayload() const;
 
   std::string getNonce();
-  [[deprecated]] static std::string getNonce(std::string_view nonceUrl);
   static std::vector<uint8_t> calculateSha256(const std::string &input);
   static std::string calculateKeyAuthorization(const std::string &token, EVP_PKEY *key);
 
@@ -131,16 +98,11 @@ private:
   static bool verifyJwtFormat(const nlohmann::json &jwt);
   static std::string loadUrlFromFile(std::string_view path);
 
-  [[deprecated]] void
-  storeUrls(std::string_view location, const std::string &challenge, const std::string &finalize)
-      const;
-
   void downloadCertificate(const std::string &certUrl) const;
 
   const ServerConfig &config_;
   const UniqueEvpKey accountPriKey_;
   const int nid_;
-  [[deprecated]] const nlohmann::json jwk_;
   const AcmeUrls acmeUrls_;
   const std::string accountUrl_;
 };
